bf55efb892754e79f70cade1356e0f65b0a99102
LeoAlioth <anzek.kosir@gmail.com>
2017-11-25 23:37:36 +0100

    basics work

--- srmclass/k1_test.py
+++ srmclass/k1_test.py
@@ -11,7 +11,7 @@ k1 = Sensor("https://k1.srm.bajtahack.si:30100")
 #k1.alloc_i2c_slave(1, 64)
 #k1.set_i2c_slave_datasize(1, 41, 16)
 # print(k1.get_i2c_slave_value(1, 41))
-# k1.init_lumniosity()
+k1.init_lumniosity()
 print(int("0x"+k1.get_lumniosity()[1:-1], 16)*100/(math.pow(16, 8)-1), "%")
 
 
