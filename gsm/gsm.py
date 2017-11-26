11da6d2fbdf21d9af39ffae06eb4dad2f1d4e850
LeoAlioth <anzek.kosir@gmail.com>
2017-11-26 05:14:22 +0100

    34

--- gsm/gsm.py
+++ gsm/gsm.py
@@ -9,7 +9,7 @@ class Gsm:
         msg = "--messaging-create-sms=\'number=\"" + number + "\",text=\"" + content + "\",smsc=\"+38641001333\",validity=100,class=1,delivery-report-request=no\'"
         print(msg)
         msg = msg.strip(" ").strip("'")
-        p = subprocess.Popen(["mmcli", "-m", "0", str(msg)], stdout=subprocess.PIPE)
+        p = subprocess.Popen(["mmcli", "-m", "0", msg], stdout=subprocess.PIPE)
         msg_id = self.check_for_new_msg()
         p = subprocess.Popen(["mmcli", "-m", "0", "-s", str(msg_id), "--send"], stdout=subprocess.PIPE)
         return None
