9bc775d101197e266f6dd6e97a088b7ee08076db
LeoAlioth <anzek.kosir@gmail.com>
2017-11-26 03:41:06 +0100

    16

--- gsm/gsm_test.py
+++ gsm/gsm_test.py
@@ -1,10 +1,13 @@
 from gsm import *
+from time import sleep
 
 p1 = Gsm("+38651884931")
 
 while p1.check_for_new_msg():
     p1.del_msg(p1.check_for_new_msg())
+    sleep(1)
 
 while 1:
     if p1.check_for_new_msg():
         print(p1.read_msg(p1.check_for_new_msg()))
+    sleep(1)
