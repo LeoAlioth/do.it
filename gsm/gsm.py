d75098c1a7173838961e5fe80cbbb4d48b31edc4
LeoAlioth <anzek.kosir@gmail.com>
2017-11-26 06:31:26 +0100

    48

--- gsm/gsm.py
+++ gsm/gsm.py
@@ -9,27 +9,6 @@ class Gsm:
             self.del_msg(self.check_for_new_msg())
         self.init_msg_check_thread()
 
-    def send_msg(self, number, content):
-        print("Sending")
-        msg = "--messaging-create-sms=number=\"" + number + "\",text=\"" + content + "\",smsc=\"+38641001333\",validity=100,class=1,delivery-report-request=no"
-        print(msg)
-
-        p = subprocess.Popen(["mmcli", "-m", "0", msg], stdout=subprocess.PIPE)
-        p.wait()
-        print("woke up")
-        msg_id = self.check_for_new_msg()
-        p = subprocess.Popen(["mmcli", "-m", "0", "-s", str(msg_id), "--send"], stdout=subprocess.PIPE)
-        p.wait()
-        return None
-
-    def read_msg(self, msg_id):
-        p = subprocess.Popen(["mmcli", "-s", str(msg_id)], stdout=subprocess.PIPE)
-        p.wait()
-        output = str(p.stdout.read(), "utf-8")
-        output = output.split("text: ")[-1].split("\n")[0].strip("'")
-        self.del_msg(msg_id)
-        return output
-
     def check_for_new_msg(self):
         p = subprocess.Popen(["mmcli", "-m", "0", "--messaging-list-sms"], stdout=subprocess.PIPE)
         p.wait()
@@ -48,12 +27,35 @@ class Gsm:
         else:
             return False
 
-    def init_msg_check_thread(self):
-        rec_t = threading.Thread(target=msg_check_thread)
-        rec_t.start()
+    def read_msg(self, msg_id):
+        p = subprocess.Popen(["mmcli", "-s", str(msg_id)], stdout=subprocess.PIPE)
+        p.wait()
+        output = str(p.stdout.read(), "utf-8")
+        output = output.split("text: ")[-1].split("\n")[0].strip("'")
+        self.del_msg(msg_id)
+        return output
+
+    def send_msg(self, number, content):
+        print("Sending")
+        msg = "--messaging-create-sms=number=\"" + number + "\",text=\"" + content + "\",smsc=\"+38641001333\",validity=100,class=1,delivery-report-request=no"
+        print(msg)
+
+        p = subprocess.Popen(["mmcli", "-m", "0", msg], stdout=subprocess.PIPE)
+        p.wait()
+        print("woke up")
+        msg_id = self.check_for_new_msg()
+        p = subprocess.Popen(["mmcli", "-m", "0", "-s", str(msg_id), "--send"], stdout=subprocess.PIPE)
+        p.wait()
+        return None
 
     def msg_check_thread(self):
         while 1:
             if self.check_for_new_msg():
-                print(self.read_msg(p1.check_for_new_msg()))
+                print(self.read_msg(self.check_for_new_msg()))
+
+    def init_msg_check_thread(self):
+        rec_t = threading.Thread(target=self.msg_check_thread)
+        rec_t.start()
+
+
 
