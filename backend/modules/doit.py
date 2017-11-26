aaf021d272dbe571a022e092b48da1c0a5766ca7
Andraž Juvan <andraz.juvan@gmail.com>
2017-11-26 01:27:58 +0100

    Added backend

---
+++ backend/modules/doit.py
@@ -0,0 +1,49 @@
+import json
+
+# Module information
+
+# Module class
+class Module:
+	def __init__(self):
+		self.mid = None
+		self.name = None
+		self.desc = None
+		self.constructor = None
+		self.destructor = None
+		self.cmds = dict()
+		self.cmds_call = dict()
+		self.cmds_cache = []
+
+	modules = dict()
+	module_cache_id = 0;
+	module_cache = []
+
+
+# Parsing function
+def cmds(cfg):
+	module = Module()
+	module.mid = Module.module_cache_id
+	module.name = cfg["module"]
+	module.desc = cfg["desc"]
+	module.constructor = cfg["constructor"]
+	module.destructor  = cfg["destructor"]
+
+	Module.module_cache.append({
+		"id": Module.module_cache_id,
+		"name": cfg["module"],
+		"desc": cfg["desc"]
+	})
+	Module.module_cache_id += 1
+
+	for command in cfg["cmds"]:
+		cmd = command["cmd"]
+		module.cmds_cache.append({
+			"name": cmd,
+			"desc": command["desc"]
+		})
+		module.cmds_call[cmd] = command["function"]
+		command.pop("cmd")
+		command.pop("function")
+		module.cmds[cmd] = command
+
+	Module.modules[module.mid] = module
---
+++ backend/rest.py
@@ -0,0 +1,43 @@
+import json
+from doit import Doit
+doit = Doit()
+
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.errorhandler
+def error404():
+	return json.dumps({
+		"status": 404,
+		"data": [],
+		"error": "Page not found!"
+	}), 404
+
+@app.route("/api/modules", methods=["GET"])
+def get_modules():
+	data = doit.get_modules()
+	return jsonify(data), data["status"]
+
+@app.route("/api/modules/<int:mid>", methods=["GET"])
+def get_module(mid=-1):
+	data = doit.get_module(mid)
+	return jsonify(data), data["status"]
+
+@app.route("/api/modules/<int:mid>/<cmd>", methods=["GET"])
+def get_module_cmds(mid=-1, cmd=""):
+	data = doit.get_module_cmds(mid, cmd)
+	return jsonify(data), data["status"]
+
+@app.route("/api/modules/<int:mid>/<cmd>", methods=["POST"])
+def get_module_cmd_data(mid=-1, cmd=""):
+	try:
+		data = json.loads(request.get_data())
+		data = doit.get_module_cmd_data(mid, cmd, data["data"])
+		return jsonify(data), data["status"]
+	except:
+		return json.dumps({
+			"status": 400,
+			"data": [],
+			"error": "Bad request"
+		})
